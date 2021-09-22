5c72f6e3adf35604dfb5d1562a8ff43f7b287bfe
Eric Martin <eric_martin@me.com>
2021-09-22 17:41:32 -0600

    updated everyting

---
+++ assets/js/highscores.js
@@ -0,0 +1,23 @@
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var restart = document.querySelector("#restart");
+
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+
+restart.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
